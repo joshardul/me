1262296af4fdaece8cb62dc67e2288f014ff8a55
Shardul Joshi <joshardul@gmail.com>
2020-12-29 12:51:55 -0500

    Prog lang added python to list of languages

--- js/helpers/education.js
+++ js/helpers/education.js
@@ -112,7 +112,7 @@ var proglang =
     name: "Programming Languages",
     code: "CSCI 4430",
     date_taken: "Fall 2020",
-    description: "Study of important concepts in current programming lanugages includeing language processing, the role of abstraction, various programming paradigms, and formal language definition. Language(s): Prolog, Scheme, Haskell, Java."
+    description: "Study of important concepts in current programming lanugages includeing language processing, the role of abstraction, various programming paradigms, and formal language definition. Language(s): Prolog, Scheme, Haskell, Java, Python."
 };
 rpi_classes.push(proglang);
 
