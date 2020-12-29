60d70a34363fe75a48853e49e5fa0417212bf76b
Shardul Joshi <joshardul@gmail.com>
2020-12-29 12:44:30 -0500

    Automate activities building with JS

--- js/builder.js
+++ js/builder.js
@@ -1,26 +1,26 @@
 // Build experiences section
-for (let i = 0; i < experiences.length; i++)
+for ( let i = 0; i < experiences.length; i++ )
 {
     // Get current experience
     var experience = experiences[i];
     
     // Create the link for the experience and add it to the list
     var link = experience_link;
-    link = link.replace("%id%", experience.id);
-    link = link.replace("%link_text%", experience.link_text);
-    $("#experience-nav").prepend(link);
+    link = link.replace( "%id%", experience.id );
+    link = link.replace( "%link_text%", experience.link_text );
+    $( "#experience-nav" ).prepend( link );
     
     // Create the card for the experience
     var card = experience_card;
-    card = card.replace("%id%", experience.id);
-    card = card.replace("%name%", experience.name);
-    card = card.replace("%location%", experience.location);
+    card = card.replace( "%id%", experience.id );
+    card = card.replace( "%name%", experience.name );
+    card = card.replace( "%location%", experience.location );
     var dates = "";
-    if (experience.start_date == experience.end_date)
+    if ( experience.start_date == experience.end_date )
     {
         dates = experience.start_date;
     }
-    else if (experience.end_date == "")
+    else if ( experience.end_date == "" )
     {
         dates = experience.start_date + " - present";
     }
@@ -28,82 +28,82 @@ for (let i = 0; i < experiences.length; i++)
     {
         dates = experience.start_date + " - " + experience.end_date;
     }
-    card = card.replace("%dates%", dates);
-    card = card.replace("%position%", experience.position);
+    card = card.replace( "%dates%", dates );
+    card = card.replace( "%position%", experience.position );
     // Create the description list
     var description = "";
-    for (let j = 0; j < experience.description.length; j++)
+    for ( let j = 0; j < experience.description.length; j++ )
     {
         description += "<li>" + experience.description[j] + "</li>"
     }
-    card = card.replace("%description%", description);
+    card = card.replace( "%description%", description );
     
     // Prepend the experience card to the experiences
-    $("#experience-cards").prepend(card);
+    $( "#experience-cards" ).prepend( card );
 }
 
 
 // Build education section
-for (let i = 0; i < schools.length; i++)
+for ( let i = 0; i < schools.length; i++ )
 {
     // Get the current school
     var school = schools[i];
     
     // Create the section for the school
     var section = school_section;
-    section = section.replace("%logo_alt%", school.logo.alt);
-    section = section.replace("%logo%", school.logo.name);
-    section = section.replace("%logo_dimensions%", school.logo.dimensions);
-    section = section.replace("%name%", school.name);
-    section = section.replace("%degree%", school.degree);
-    section = section.replace("%location%", school.location);
-    section = section.replace("%grad_year%", school.grad_year);
-    section = section.replace("%gpa%", school.gpa);
+    section = section.replace( "%logo_alt%", school.logo.alt );
+    section = section.replace( "%logo%", school.logo.name );
+    section = section.replace( "%logo_dimensions%", school.logo.dimensions );
+    section = section.replace( "%name%", school.name );
+    section = section.replace( "%degree%", school.degree );
+    section = section.replace( "%location%", school.location );
+    section = section.replace( "%grad_year%", school.grad_year );
+    section = section.replace( "%gpa%", school.gpa );
     // Create additional details list
     var additional_details = "";
-    for (let j = 0; j < school.details.length; j++)
+    for ( let j = 0; j < school.details.length; j++ )
     {
         additional_details += school.details[j];
-        if (j < school.details.length - 1)
+        if ( j < school.details.length - 1 )
         {
             additional_details += "<br>";
         }
     }
-    section = section.replace("%additional_details%", additional_details);
-    section = section.replace("%catalog%", school.catalog);
-    section = section.replaceAll("%id%", school.id);
+    section = section.replace( "%additional_details%", additional_details );
+    section = section.replace( "%catalog%", school.catalog );
+    section = section.replaceAll( "%id%", school.id );
     
     // Create the modal for the school
     var modal = school_modal;
-    modal = modal.replaceAll("%id%", school.id);
-    modal = modal.replace("%abbreviation%", school.abbreviation);
-    modal = modal.replace("%catalog%", school.catalog);
+    modal = modal.replaceAll( "%id%", school.id );
+    modal = modal.replace( "%abbreviation%" , school.abbreviation );
+    modal = modal.replace( "%catalog%" , school.catalog );
     
     // Prepend the school section and modal to the schools
-    $("#schools").prepend(section + modal);
+    $( "#schools" ).prepend( section + modal );
     
-    // Create courses description list (done after added the school so it can be appended to the HTML element with the id)
-    for (let j = 0; j < school.courses.length; j++)
+    // Create courses description list (done after adding the school so list can be appended to the HTML element with the id)
+    for ( let j = 0; j < school.courses.length; j++ )
     {
         // Get the current course
         var course = school.courses[j];
         
         // Create the block for the course
         var block = course_block;
-        block = block.replace("%name%", course.name);
-        block = block.replace("%code%", course.code);
-        block = block.replace("%date_taken%", course.date_taken);
-        block = block.replace("%description%", course.description);
+        block = block.replace( "%name%", course.name );
+        block = block.replace( "%code%", course.code );
+        block = block.replace( "%date_taken%", course.date_taken );
+        block = block.replace( "%description%", course.description );
         
         // Append the course to the notable courses modal
         var modal_block = course_modal_block;
-        modal_block = modal_block.replace("%course_block%", block);
-        $("#" + school.id + "-notable-list").append(modal_block);
+        modal_block = modal_block.replace( "%course_block%", block );
+        $( "#" + school.id + "-notable-list" ).append( modal_block );
         
         // If one of the first three courses, append it to top three section
-        if (j < 3)
+        if ( j < 3 )
         {
-            $("#" + school.id + "-top-three").append(block);
+            $( "#" + school.id + "-top-three" ).append( block );
         }
     }
 }
@@ -111,53 +111,117 @@ for (let i = 0; i < schools.length; i++)
 // TODO: Build projects section
 
 // Build skills section
-for (let i = 0; i < skill_types.length; i++)
+for ( let i = 0; i < skill_types.length; i++ )
 {
     // Get the current skill type
     var skill_type = skill_types[i];
     
     // Create the filter control
     var filter_control = type_filter_control;
-    filter_control = filter_control.replace("%filter%", skill_type.filter);
-    filter_control = filter_control.replace("%name%", skill_type.name);
+    filter_control = filter_control.replace( "%filter%", skill_type.filter );
+    filter_control = filter_control.replace( "%name%", skill_type.name );
     
     // Append the filter control to list of filter controls
     $("#skill-filter-controls").append(filter_control);
     
     // Add skills chips
-    for (let j = 0; j < skill_type.skills.length; j++)
+    for ( let j = 0; j < skill_type.skills.length; j++ )
     {
         // Get the current skill
         var skill = skill_type.skills[j];
         
         // Create the skill chip
         var chip = skill_chip;
-        chip = chip.replace("%type%", skill_type.filter);
-        chip = chip.replace("%skill%", skill);
+        chip = chip.replace( "%type%", skill_type.filter );
+        chip = chip.replace( "%skill%", skill );
         
         // Add the skill chip to the list of skill chips
-        $("#skill-chips").append(chip);
+        $( "#skill-chips" ).append( chip );
     }
 }
 
+//<li class="%class%">
+//    <a class="uk-accordion-title" href="#">%organization%%sub_title%</a>
+//    <div class="uk-accordion-content">
+//        <p class="justify">%description%</p>
+//        <table class="uk-table uk-table-divider">
+//            <thead>
+//                <tr>
+//                    <th>Positions held</th>
+//                    <th>Time period</th>
+//                </tr>
+//            </thead>
+//            <tbody id="%id%-positions-table">
+//                
+//            </tbody>
+//        </table>
+//    </div>
+//</li>
+
 // TODO: Build activities section
+for ( let i = 0; i < activities.length; i++ )
+{
+    // Get the current school
+    var activity = activities[i];
+    
+    // Create the section for the school
+    var accordion_item = activity_accordion_item;
+    // If first course, set class to uk-open, else add nothing
+    accordion_item = accordion_item.replace( "%class%", ( i == 0 ) ? "uk-open" : "" );
+    accordion_item = accordion_item.replace( "%organization%", activity.organization );
+    // If there is a sub-title, add it, else add nothing
+    accordion_item = accordion_item.replace( "%sub_title%", ( activity.sub_title != "" ) ? " (" + activity.sub_title + ")" : activity.sub_title );
+    accordion_item = accordion_item.replace( "%description%", activity.description );
+    accordion_item = accordion_item.replaceAll( "%id%", activity.id );
+    
+    // Append the activity accordion item to the activities accordion
+    $( "#activities-accordion" ).append( accordion_item );
+    
+    // Create positions list (done after adding the activity so list can be appended to the HTML element with the id)
+    for ( let j = 0; j < activity.positions.length; j++ )
+    {
+        // Get the current course
+        var position = activity.positions[j];
+        
+        // Create the block for the course
+        var row = activity_position_row;
+        row = row.replace( "%title%", position.title );
+        var dates = "";
+        if ( position.start_date == position.end_date )
+        {
+            dates = position.start_date;
+        }
+        else if ( position.end_date == "" )
+        {
+            dates = position.start_date + " - present";
+        }
+        else
+        {
+            dates = position.start_date + " - " + position.end_date;
+        }
+        row = row.replace( "%dates%", dates );
+        
+        // Append the position to the positions table
+        $( "#" + activity.id + "-positions-table" ).append( row );
+    }
+}
 
 // Build honors/awards section
-for (let i = 0; i < honors.length; i++)
+for ( let i = 0; i < honors.length; i++ )
 {
     // Get the current honor
     var honor = honors[i];
     
     // Create the row for the table
     var row = honor_row;
-    row = row.replace("%title%", honor.title);
+    row = row.replace( "%title%", honor.title );
     var levels = "" + honor.levels;
-    levels = levels.replaceAll(",", ", ");
-    row = row.replace("%levels%", levels);
+    levels = levels.replaceAll( ",", ", " );
+    row = row.replace( "%levels%", levels );
     var dates = "" + honor.dates;
-    dates = dates.replaceAll(",", ", ");
-    row = row.replace("%dates%", dates);
+    dates = dates.replaceAll( ",", ", " );
+    row = row.replace( "%dates%", dates );
     
     // Add the row to the honors table
-    $("#honors-table").prepend(row);
+    $( "#honors-table" ).prepend( row );
 }
---
+++ js/helpers/activities.js
@@ -0,0 +1,124 @@
+// Activities helpers
+
+// Activities objects
+var activities = [{}];
+activities.pop(); // Can't figure out how to make this array know it is an array of objects without this line
+
+// The Polytechnic
+var poly =
+{
+    id: "poly",
+    organization: "<i>The Polytechnic</i>",
+    sub_title: "school newspaper",
+    description: "<i>The Polytechnic</i>, or <i>The Poly</i>, for short, is the online school newspaper of RPI, found at <a href=\"https://poly.rpi.edu\">poly.rpi.edu</a>. We help keep the student body informed of campus news by covering events, conducting interviews, and more. Additionally, staff and general public can write opinion pieces to express arguments that they feel are important. We have several departments: News, Features, Editorial/Opinion, Sports, Photo, Publicity, and Copy. Elections for voting member positions are done at the end of each semester for the following one. Joining <i>The Poly</i> has been a great decision for me. I have began to enjoy writing in recent years, but I still find that I consider other subject priorities. At a STEM school where the humanities are less emphasized, I knew I would not pursue them in a classroom setting, so I wanted to find a way to keep up my writing. <i>The Poly</i> provided me with that opportunity, as well as introduced me to a great group of people who shared my combined interest in both technology and writing. My staff page, <a href=\"https://poly.rpi.edu/staff/shardul-joshi/\">poly.rpi.edu/staff/shardul-joshi/</a>, lists all of my articles for <i>The Poly</i>.",
+    positions:
+    [
+        {
+            title: "Editorial/Opinion Editor",
+            start_date: "May 2020",
+            end_date: ""
+        },
+        {
+            title: "Associate Editorial/Opinion Editor",
+            start_date: "Feb 2020",
+            end_date: "May 2020"
+        },
+        {
+            title: "Editorial/Opinion Coordinator",
+            start_date: "Dec 2019",
+            end_date: "Feb 2020"
+        },
+        {
+            title: "Associate News Editor",
+            start_date: "Sep 2019",
+            end_date: "Dec 2019"
+        }
+    ]
+};
+activities.push(poly);
+
+// Spoonful of Sarcasm
+var sofs =
+{
+    id: "sofs",
+    organization: "Spoonful of Sarcasm",
+    sub_title: "",
+    description: "I have been playing guitar since 6th grade, having taken lessons from then until I left for college. In freshman year of high school, a group of friends and I decided to join an ensemble at our common music school, which was organizing these small bands and providing them space to practice and and mentor. This group of 5 grew closer over the years, and we began to play more and more often, and for more and more public audiences. We have played at the Hard Rock Cafe (Boston), Harvard Stadium, Gillette Stadium, and many open mic nights in our local area. We have also played live on WEMF Radio. Eventually, we were writing our own songs, too, and have even published a few on all major streaming services, such as <a href=\"https://open.spotify.com/artist/0tZWPXIaNA3yNrekm7xJPB?si=fxS7-iREQO2z1iU_1j3oIQ\">Spotify</a> and <a href=\"https://music.apple.com/us/artist/spoonful-of-sarcasm/1406403423\">Apple Music</a>. Learn more about us at <a href=\"http://spoonfulofsarcasm.tk\">spoonfulofsarcasm.tk</a>.",
+    positions:
+    [
+        {
+            title: "Rhythm guitarist",
+            start_date: "Sep 2015",
+            end_date: "Jun 2019"
+        }
+    ]
+};
+activities.push(sofs);
+
+// Cross Country (high school)
+var xc =
+{
+    id: "xc",
+    organization: "Cross Country",
+    sub_title: "high school",
+    description: "In the days leading up to the start of high school, excitedly, I tried out for the freshman soccer team. With limited experience in soccer, unsurprisingly, I did not qualify for the team. In retrospect, getting cut from that team worked in my favor; two days later, crestfallen and rather reluctantly, I joined the cross-country team. When my dad dropped me off at the first practice, I fully expected to hate the sport and quit the very next day. I could not have been more wrong – I ran three miles that day and quickly became captivated with the runner’s high. Over the next four seasons, I made great friendships and found a strong comraderie in the team that I thoroughly enjoyed being a part of. I was not very good, but I kept running and was even elected captain at the end of my junior year, giving me the opportunity to do for the new freshman what previous captains had done for me.",
+    positions:
+    [
+        {
+            title: "Captain (JV &amp; Varsity)",
+            start_date: "Fall 2018",
+            end_date: "Fall 2018"
+        },
+        {
+            title: "General JV team member",
+            start_date: "Fall 2015, 2016, 2017",
+            end_date: "Fall 2015, 2016, 2017"
+        }
+    ]
+};
+activities.push(xc);
+
+// Computer Science Club (high school)
+var csclubhs =
+{
+    id: "csclubhs",
+    organization: "Computer Science Club",
+    sub_title: "high school",
+    description: "Our computer science club in high school met weekly for many various activities. Sometimes it was taking the tests for the <a href=\"http://www.acsl.org\">American Computer Science League</a> competition or preparing for them. Other times we each worked on our own computer science projects, personal or academic, helping each other with bugs. A few times we also took it upon ourselves to learn something new together, from a new language to how to tackle a certain challenge. We even spent some meetings brainstorming potential ideas for projects we would want to do sometime, and thought of the various technologies we would need to complete it. We also participated as a team in cybersecurity capture-the-flags, programming competitions, and hackathons. It was a very small club, with no leadership positions.",
+    positions:
+    [
+        {
+            title: "Member",
+            start_date: "Sep 2017",
+            end_date: "June 2019"
+        }
+    ]
+};
+activities.push(csclubhs);
+
+// Science Team (high school)
+var scienceteam =
+{
+    id: "science-team",
+    organization: "Science Team",
+    sub_title: "high school",
+    description: "At the beginning of my junior year, my friend, who was captain of our school's science team, encouraged me to join the team. The team was part of the <a href=\"https://sites.google.com/site/northshorescienceleague/\">North Shore Science League</a>, which was made up of teams from about 20 high schools and held monthly meets throughout the school year. Three different events occurred at each meet, and each school team could send up to three four-person groups to each event – A, B, and C teams. Only the A team would score for the school in the event. Whichever school scored the most points across all three events would win the meet, and at the end of the school year, the schools would be ranked for an overall year's awards. After attending my first meet, my event team, which was the A-team for my school for the event, placed very high, missing first place by just a few points. Throughout the year, I continued to contribute in whatever way I could to help the team ultimately place third at the end of the year. The team leadership and teacher advisors then encouraged me to apply for a leadership position for the following school year, for which they then selected me from the pool of applicants. As Logistics Coordinator, I was in charge of managing funds, obtaining materials for our various events, and team purchases (food, materials, etc.). I held this position for my senior year, and we placed third in the league once again at the end of the year.",
+    positions:
+    [
+        {
+            title: "Member",
+            start_date: "Sep 2017",
+            end_date: "Jun 2018"
+        },
+        {
+            title: "Logistics Coordinator",
+            start_date: "Jun 2018",
+            end_date: "Jun 2019"
+        }
+    ]
+};
+activities.push(scienceteam);
+
+// HTML Elements
+var activity_accordion_item = "<li class=\"%class%\"><a class=\"uk-accordion-title\" href=\"#\">%organization%%sub_title%</a><div class=\"uk-accordion-content\"><p class=\"justify\">%description%</p><table class=\"uk-table uk-table-divider\"><thead><tr><th>Positions held</th><th>Time period</th></tr></thead><tbody id=\"%id%-positions-table\"></tbody></table></div></li>";
+var activity_position_row = "<tr><td>%title%</td><td>%dates%</td></tr>";
